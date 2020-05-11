95005b925ff0c3b37501e3677ef65fd6cfc0da25
Mythruss <61095312+Mythruss@users.noreply.github.com>
2020-05-11 18:11:41 -0300

    Update node.h

--- node.h
+++ node.h
@@ -25,7 +25,7 @@
  *  (b) Remove edgeWeight, which is used nowhere.
  * May 11, 2020 (IC V1.6)
  *  (a) Changed logicalDotsPerInchX variable to physicalDotsPerInchX
- *  to correct scaling issues. (Only reliable with Qt V5.12.2 or higher)
+ *  to correct scaling issues. (Only reliable with Qt V5.14.2 or higher)
  *  (b) Removed unused physicalDotsPerInchY variable as only one DPI
  *  value is needed for the node's radius.
  */
